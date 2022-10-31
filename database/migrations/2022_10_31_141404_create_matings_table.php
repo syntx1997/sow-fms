248d2a33366904fc83276eac5139d771e7a09ce4
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2022-10-31 22:21:14 +0800

    Created Migration and Model for Mating

---
+++ database/migrations/2022_10_31_141404_create_matings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('litter_no');
+            $table->date('date');
+            $table->string('boar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matings');
+    }
+};
