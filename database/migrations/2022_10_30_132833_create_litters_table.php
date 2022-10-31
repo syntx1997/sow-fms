e0bb9773cabc305ffde36ffe65f4d34c33883c28
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2022-10-31 22:12:14 +0800

    Update Migration

--- database/migrations/2022_10_30_132833_create_litters_table.php
+++ database/migrations/2022_10_30_132833_create_litters_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('litters', function (Blueprint $table) {
             $table->id();
+            $table->string('litter_no');
             $table->foreignId('sow_id');
             $table->timestamps();
         });
