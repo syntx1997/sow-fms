7f8d187e9573aec6637dfe6886fb5ab38fb6ab50
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2022-11-17 20:20:08 +0800

    Updates

--- resources/views/pages/admin/pig-management.blade.php
+++ resources/views/pages/admin/pig-management.blade.php
@@ -31,39 +31,6 @@
                 </div>
             </div>
         </div>
-        <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">Light Gallery</h4>
-            </div>
-            <div class="card-body pb-1">
-                <div id="lightgallery" class="row">
-                    <a href="images/big/img1.jpg" data-exthumbimage="images/big/img1.jpg" data-src="images/big/img1.jpg" class="col-lg-3 col-md-6 mb-4">
-                        <img src="images/big/img1.jpg" alt="" style="width:100%;">
-                    </a>
-                    <a href="images/big/img2.jpg" data-exthumbimage="images/big/img2.jpg" data-src="images/big/img2.jpg" class="col-lg-3 col-md-6 mb-4">
-                        <img src="images/big/img2.jpg" alt="" style="width:100%;">
-                    </a>
-                    <a href="images/big/img3.jpg" data-exthumbimage="images/big/img3.jpg" data-src="images/big/img3.jpg" class="col-lg-3 col-md-6 mb-4">
-                        <img src="images/big/img3.jpg" alt="" style="width:100%;">
-                    </a>
-                    <a href="images/big/img4.jpg" data-exthumbimage="images/big/img4.jpg" data-src="images/big/img4.jpg" class="col-lg-3 col-md-6 mb-4">
-                        <img src="images/big/img4.jpg" alt="" style="width:100%;">
-                    </a>
-                    <a href="images/big/img5.jpg" data-exthumbimage="images/big/img5.jpg" data-src="images/big/img5.jpg" class="col-lg-3 col-md-6 mb-4">
-                        <img src="images/big/img5.jpg" alt="" style="width:100%;">
-                    </a>
-                    <a href="images/big/img6.jpg" data-exthumbimage="images/big/img6.jpg" data-src="images/big/img6.jpg" class="col-lg-3 col-md-6 mb-4">
-                        <img src="images/big/img6.jpg" alt="" style="width:100%;">
-                    </a>
-                    <a href="images/big/img7.jpg" data-exthumbimage="images/big/img7.jpg" data-src="images/big/img7.jpg" class="col-lg-3 col-md-6 mb-4">
-                        <img src="images/big/img7.jpg" alt="" style="width:100%;">
-                    </a>
-                    <a href="images/big/img8.jpg" data-exthumbimage="images/big/img8.jpg" data-src="images/big/img8.jpg" class="col-lg-3 col-md-6 mb-4">
-                        <img src="images/big/img8.jpg" alt="" style="width:100%;">
-                    </a>
-                </div>
-            </div>
-        </div>
     </div>
 
     <div id="addPigModal" class="modal fade" tabindex="-1">
