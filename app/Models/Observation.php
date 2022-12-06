f332b65c3409b3234dc8845298871235e2de5b45
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2022-12-06 16:53:16 +0800

    Added New Migration: Observations

--- app/Models/Observation.php
+++ app/Models/Observation.php
@@ -5,19 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Pig extends Model
+class Observation extends Model
 {
     use HasFactory;
     protected $fillable = [
         'pig_no',
-        'breed',
-        'date_born',
-        'origin',
-        'dam',
-        'date_procured',
-        'sire',
-        'type',
-        'photo',
-        'under_observation'
+        'treatment',
+        'feed_amount',
+        'feed_type',
+        'start_weight',
+        'end_weight',
+        'status'
     ];
 }
---
+++ database/migrations/2022_12_06_084452_create_observations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('observations', function (Blueprint $table) {
+            $table->id();
+            $table->string('pig_no');
+            $table->string('treatment');
+            $table->string('feed_amount');
+            $table->string('feed_type');
+            $table->string('start_weight');
+            $table->string('end_weight');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('observations');
+    }
+};
